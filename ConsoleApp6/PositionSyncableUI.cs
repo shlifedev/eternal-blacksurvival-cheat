ed64eddcc69d6c29f233413a4dbba42333142d1a
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-11-25 12:57:52 +0900

    Create PositionSyncableUI.cs

---
+++ ConsoleApp6/PositionSyncableUI.cs
@@ -0,0 +1,33 @@
+﻿using Blis.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using BIFog;
+using Blis.Client.UI;
+using Blis.Common;
+using Blis.Common.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Blis.Client.Cheat
+{
+    public class PositionSyncableUI : MonoBehaviour
+    {
+        public LocalObject followTarget;
+        public Vector3 worldOffset;
+
+        public void Init(LocalObject obj)
+        {
+            this.followTarget = obj;
+            this.transform.SetParent(CheatCanvas.instance.canvas.transform, false);
+        }
+        public void Update()
+        {
+            if (followTarget != null)
+            {
+                this.transform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, followTarget.GetPosition() + worldOffset);
+            }
+        }
+    }
+}
