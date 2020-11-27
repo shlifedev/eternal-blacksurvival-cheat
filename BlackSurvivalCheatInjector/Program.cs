275e394310d5123a1ffcfda1a0de5f0d12a184d2
Zero is Black <49047211+shlifedev@users.noreply.github.com>
2020-11-27 17:20:35 +0900

    Update Program.cs

--- BlackSurvivalCheatInjector/Program.cs
+++ BlackSurvivalCheatInjector/Program.cs
@@ -8,6 +8,7 @@ namespace BlackSurvivalCheatInjector
 {
     /// <summary>
     /// 추후 NLogConfig막히면 함수 시그니처로 Call하는 식으로 변경
+    /// 혹은 게임이 켜지고(해시체크끝난뒤) NLogConfig 읽기전에 파일 갈아치우는 코드로 변경
     /// </summary>
     class Program
     {
